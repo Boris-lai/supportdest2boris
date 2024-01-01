f9a3aa813eb522ebb427a7b2d4ed7586422d7a51
Boris <borislai@BorisdeMacBook-Pro.local>
2024-01-01 16:50:24 +0800

    prepare for deploy

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,7 @@
 import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
 import express from "express";
 import colors from "colors";
 import dotenv from "dotenv";
